d34e53ac0d8a175f8bdecc77b1597fe5c6a8539e
Daniel Khashev (Angelhe1m) <116936326+angelhe1m@users.noreply.github.com>
2023-02-26 00:41:43 +0300

    Update alg.cpp
    beta3

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,45 +4,36 @@
 
 
 bool checkPrime(uint64_t value) {
-if (value <= 1)
-    return false;
-  for (int k = 2; k * k <= value; k++) {
-    if (value % k == 0)
-      return false;
-  }
-  return true;
+    uint64_t del = 2;
+    while (del * del <= value) {
+        if (value % del == 0) return false;
+        del++;
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-uint64_t chsl = 0;
- uint64_t nomer = 0;
-  while (n > 0) { 
-    if (checkPrime(chsl) == 1) {
-      nomer = chsl;
-      --n;
+    uint64_t index = 0;
+    uint64_t number = 2;
+    while (index < n) {
+        if (checkPrime(number++)) index++;
     }
-    ++chsl;
-  }
-  return nomer;
+    return number - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
- while (true) {
-    value++;
-    if (checkPrime(value)) {
-      return value;
-    }
-  }
+    while (!checkPrime(++value)) {}
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-uint64_t summa = 0;
-  hbound = hbound - 1;
-  while (hbound > 0) {
-    if (checkPrime(hbound)) {
-      summa = summa + hbound;
+    uint64_t number = 2;
+    uint64_t sum = 0;
+    while (number < hbound) {
+        if (checkPrime(number)) {
+            sum += number;
+        }
+        number++;
     }
-    --hbound;
-  }
-  return summa;
+    return sum;
 }
