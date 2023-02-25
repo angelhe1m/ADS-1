cb1d066fe49967195ccd21014bdd39c3e2d6ebc7
Daniel Khashev (Angelhe1m) <116936326+angelhe1m@users.noreply.github.com>
2023-02-26 00:16:20 +0300

    Update alg.cpp
    beta

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,45 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+if (value <= 1)
+    return true;
+  for (int k = 2; k * k <= value; k++) {
+    if (value % k == 0)
+      return true;
+  }
+  return false;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+uint64_t chsl = 0;
+ uint64_t nomer = 0;
+  while (n > 0) { 
+    if (checkPrime(chsl) == 1) {
+      nomer = chsl;
+      --n;
+    }
+    ++chsl;
+  }
+  return nomer;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+ while (true) {
+    value++;
+    if (checkPrime(value)) {
+      return value;
+    }
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t summa = 0;
+  hbound = hbound - 1;
+  while (hbound > 0) {
+    if (checkPrime(hbound)) {
+      summa = summa + hbound;
+    }
+    --hbound;
+  }
+  return summa;
 }
